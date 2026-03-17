d67e75f73354579c35634da9e2c8ac204f8ca6fe
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-03-17 20:36:16 +0530

    Fixed the workflow to render task-definition

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -4,7 +4,7 @@ export default {
   config: {
     translations: {
       en: {
-        "app.components.HomePage.welcome.title": "Hey Shivam, Welcome Back!",
+        "app.components.HomePage.welcome.title": "Hey Shivam, Welcome Back to Strapi",
         "app.components.HomePage.welcome.subtitle": "This is your custom administration panel.",
         "app.components.HomePage.button.blog": "View Documentation",
       },
